58ddb92d63280766907685e98822d469020e1339
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2020-11-17 19:42:50 +0100

    State variables for MILP must be considered separately.

---
+++ src/JuMP.jl
@@ -0,0 +1,48 @@
+function JuMP.build_variable(
+    _error::Function,
+    info::JuMP.VariableInfo,
+    ::Type{State};
+    initial_value = NaN,
+    kwargs...,
+)
+    if isnan(initial_value)
+        _error(
+            "When creating a state variable, you must set the " *
+            "`initial_value` keyword to the value of the state variable at" *
+            " the root node.",
+        )
+    end
+    return StateInfo(
+        JuMP.VariableInfo(
+            false,
+            NaN,  # lower bound
+            false,
+            NaN,  # upper bound
+            false,
+            NaN,  # fixed value
+            false,
+            NaN,  # start value
+            false,
+            false, # binary and integer
+        ),
+        info,
+        initial_value,
+        kwargs,
+    )
+end
+
+function JuMP.add_variable(problem::JuMP.Model, state_info::StateInfo, name::String)
+    state = State(
+        JuMP.add_variable(problem, JuMP.ScalarVariable(state_info.in), name * "_in"),
+        JuMP.add_variable(problem, JuMP.ScalarVariable(state_info.out), name * "_out"),
+    )
+    integrality_handler = get_integrality_handler(problem)
+    setup_state(problem, state, state_info, name, integrality_handler)
+    return state
+end
+
+JuMP.variable_type(model::JuMP.Model, ::Type{State}) = State
+
+function JuMP.value(state::State{JuMP.VariableRef})
+    return State(JuMP.value(state.in), JuMP.value(state.out))
+end
---
+++ src/state.jl
@@ -0,0 +1,83 @@
+struct State{T}
+    # The incoming state variable.
+    in::T
+    # The outgoing state variable.
+    out::T
+    # The lower bound
+    lb::Float64
+    # The upper bound
+    ub::Float64
+
+    function State(
+        in::T,
+        out::T
+         ) where{T}
+        return new(
+            in,
+            out,
+            -Inf,
+            Inf
+        )
+    end
+end
+
+struct StateInfo
+    in::JuMP.VariableInfo
+    out::JuMP.VariableInfo
+    initial_value::Float64
+    kwargs
+end
+
+function setup_state(
+    subproblem::JuMP.Model,
+    state::State,
+    state_info::StateInfo,
+    name::String,
+    ::ContinuousRelaxation,
+)
+    node = get_node(subproblem)
+    sym_name = Symbol(name)
+    @assert !haskey(node.states, sym_name)  # JuMP prevents duplicate names.
+    node.ext[:lin_states][sym_name] = state
+    graph = get_policy_graph(subproblem)
+    graph.ext[:lin_initial_root_state][sym_name] = state_info.initial_value
+    return
+end
+
+# Internal function: set the incoming state variables of node to the values
+# contained in state.
+function set_incoming_state(node::Node, state::Dict{Symbol,Float64})
+    for (state_name, value) in state
+        JuMP.fix(node.ext.[:lin_states][state_name].in, value)
+    end
+    return
+end
+
+# Internal function: get the values of the outgoing state variables in node.
+# Requires node.subproblem to have been solved with PrimalStatus ==
+# FeasiblePoint.
+function get_outgoing_state(node::Node)
+    values = Dict{Symbol,Float64}()
+    for (name, state) in node.ext.[:lin_states]
+        # To fix some cases of numerical infeasiblities, if the outgoing value
+        # is outside its bounds, project the value back onto the bounds. There
+        # is a pretty large (×5) penalty associated with this check because it
+        # typically requires a call to the solver. It is worth reducing
+        # infeasibilities though.
+        outgoing_value = JuMP.value(state.out)
+        if JuMP.has_upper_bound(state.out)
+            current_bound = JuMP.upper_bound(state.out)
+            if current_bound < outgoing_value
+                outgoing_value = current_bound
+            end
+        end
+        if JuMP.has_lower_bound(state.out)
+            current_bound = JuMP.lower_bound(state.out)
+            if current_bound > outgoing_value
+                outgoing_value = current_bound
+            end
+        end
+        values[name] = outgoing_value
+    end
+    return values
+end
