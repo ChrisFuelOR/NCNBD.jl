362f871b3a40f5adf56a87f6cc36038f30c59b1b
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2021-01-04 20:27:59 +0100

    Infiltrate

--- src/piecewiseLinearRelaxation.jl
+++ src/piecewiseLinearRelaxation.jl
@@ -526,6 +526,7 @@ function piecewise_linear_refinement(model::SDDP.PolicyGraph{T}, appliedSolvers:
                     # delete old simplex
                     deleteat!(nlFunction.triangulation.simplices, simplex_index)
                     # adapt the indices of the new simplices accordingly
+                    @infiltrate
                     for i in 1:size(new_simplex_indices_list,1)
                         new_index = new_simplex_indices_list[i]
                         if new_index > simplex_index
