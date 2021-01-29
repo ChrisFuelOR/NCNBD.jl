0c54689198a040933a10f8c4e6cb884f677a9318
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2021-01-29 21:14:43 +0100

    Test

--- src/algorithm_inner.jl
+++ src/algorithm_inner.jl
@@ -639,6 +639,7 @@ function solve_subproblem_backward(
         variable_name = node.ext[:backward_data][:bin_states][name]
         reference_to_constr = FixRef(variable_name)
         dual_vars_initial[i] = JuMP.getdual(reference_to_constr)
+        value = JuMP.value(variable_name)
         @infiltrate algoParams.infiltrate_state in [:all, :inner]
     end
 
