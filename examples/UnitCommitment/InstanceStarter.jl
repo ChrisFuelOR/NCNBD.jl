71a78b2f0158a4ec1f294552f0d71b7de51a66a6
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2021-02-03 21:12:31 +0100

    Changed all instances such that > they can be started directly with defining the parameters within the module > they can be started from another script by setting the parameters there
    Implemented a script InstanceStarter to start several instances after 
    each other

---
+++ examples/UnitCommitment/InstanceStarter.jl
@@ -0,0 +1,70 @@
+using JuMP
+using SDDP
+using NCNBD
+using Revise
+using Gurobi
+using GAMS
+#using SCIP
+using Infiltrator
+
+
+function start_instances()
+
+    # INSTANCE DEFINITIONS
+    parameter_sets = [
+                      [1e-8, 1e-8, :none, :kelley, 0.0],
+                      [1e-8, 1e-8, :none, :bundle_level, 0.4],
+                      [1e-8, 1e-8, :none, :bundle_level, 0.2],
+                      [1e-8, 1e-8, :none, :bundle_level, 0.6],
+                      [1e-8, 1e-8, :cplex_combi, :kelley, 0.0],
+                      [1e-8, 1e-8, :cplex_combi, :bundle_level, 0.4],
+                      [1e-8, 1e-8, :cplex_combi, :bundle_level, 0.2],
+                      [1e-8, 1e-8, :cplex_combi, :bundle_level, 0.6],
+                      [1e-4, 1e-4, :none, :kelley, 0.0],
+                      [1e-4, 1e-4, :none, :bundle_level, 0.4],
+                      [1e-4, 1e-4, :none, :bundle_level, 0.2],
+                      [1e-4, 1e-4, :none, :bundle_level, 0.6],
+                      [1e-4, 1e-4, :cplex_combi, :kelley, 0.0],
+                      [1e-4, 1e-4, :cplex_combi, :bundle_level, 0.4],
+                      [1e-4, 1e-4, :cplex_combi, :bundle_level, 0.2],
+                      [1e-4, 1e-4, :cplex_combi, :bundle_level, 0.6],
+                      ]
+
+    for parameter_set in parameter_sets
+
+        lagrangian_atol = parameter_set[1]
+        lagrangian_rtol = parameter_set[2]
+        dual_initialization_regime = parameter_set[3]
+        lagrangian_method = parameter_set[4]
+        level_factor = parameter_set[5]
+
+        unitCommitment_1_10_with_parameters(
+            lagrangian_atol=lagrangian_atol, lagrangian_rtol=lagrangian_rtol,
+            dual_initialization_regime=dual_initialization_regime
+            lagrangian_method=lagrangian_method, level_factor=level_factor
+        )
+        unitCommitment_2_2_with_parameters(
+            lagrangian_atol=lagrangian_atol, lagrangian_rtol=lagrangian_rtol,
+            dual_initialization_regime=dual_initialization_regime
+            lagrangian_method=lagrangian_method, level_factor=level_factor
+        )
+        unitCommitment_2_5_with_parameters(
+            lagrangian_atol=lagrangian_atol, lagrangian_rtol=lagrangian_rtol,
+            dual_initialization_regime=dual_initialization_regime
+            lagrangian_method=lagrangian_method, level_factor=level_factor
+        )
+        unitCommitment_2_10_with_parameters(
+            lagrangian_atol=lagrangian_atol, lagrangian_rtol=lagrangian_rtol,
+            dual_initialization_regime=dual_initialization_regime
+            lagrangian_method=lagrangian_method, level_factor=level_factor
+        )
+        unitCommitment_5_5_with_parameters(
+            lagrangian_atol=lagrangian_atol, lagrangian_rtol=lagrangian_rtol,
+            dual_initialization_regime=dual_initialization_regime
+            lagrangian_method=lagrangian_method, level_factor=level_factor
+        )
+
+    end
+end
+
+start_instances()
