1f9645b688dc7a29cb4da0408de8846b8fb630df
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2021-02-12 21:16:05 +0100

    Further infiltrates

--- src/algorithm_inner.jl
+++ src/algorithm_inner.jl
@@ -22,6 +22,8 @@ function inner_loop_iteration(
         forward_trajectory = NCNBD.inner_loop_forward_pass(model, options, algoParams, appliedSolvers, options.forward_pass)
     end
 
+    @infiltrate || model.ext[:sddp_policy_graph].ext[:iteration] == 13
+
     # BINARY REFINEMENT
     ############################################################################
     # If the forward pass solution did not change during the last iteration, then
@@ -37,7 +39,7 @@ function inner_loop_iteration(
         end
     end
 
-    @infiltrate algoParams.infiltrate_state in [:all, :inner]
+    @infiltrate algoParams.infiltrate_state in [:all, :inner] || model.ext[:sddp_policy_graph].ext[:iteration] == 13
 
     # BACKWARD PASS
     ############################################################################
