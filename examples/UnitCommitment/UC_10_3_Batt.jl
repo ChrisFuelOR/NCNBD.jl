a0cf343b9859185fb74d9b96218edf88f13171c2
Christian Fuellner <christian.fuellner@kit.edu>
2021-03-01 23:36:01 +0100

    Corrected an error

--- examples/UnitCommitment/UC_10_3_Batt.jl
+++ examples/UnitCommitment/UC_10_3_Batt.jl
@@ -348,7 +348,7 @@ function define_10_3()
             JuMP.@constraint(problem, dischargelevel[i=1:num_of_batteries], discharge[i] <= level[i].in + charge[i])
             JuMP.@constraint(problem, soc_eq[i=1:num_of_batteries], soc[i] == level[i].in / batteries[i].max_level)
 
-            if t == num_of_stages:
+            if t == num_of_stages
                 JuMP.@constraint(problem, end_level[i=1:num_of_batteries] level[i].out == batteries[i].level_end)
             end
 
