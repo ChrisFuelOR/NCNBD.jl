00863d10a3eed0a64c2a8bd2f76bcac67471bb1b
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2021-03-04 14:48:25 +0100

    Different test

--- examples/UnitCommitment/UC_10_3_Batt.jl
+++ examples/UnitCommitment/UC_10_3_Batt.jl
@@ -64,7 +64,7 @@ function unitCommitment()
 
     # define infiltration level
     # TODO: Abstract data type
-    infiltrate_state = :none
+    infiltrate_state = :inner
     # alternatives: :none, :all, :outer, :sigma, :inner, :lagrange, :bellman
 
     # define regime for initializing duals for Lagrangian relaxation
@@ -72,7 +72,7 @@ function unitCommitment()
     # alternatives: :zeros, :gurobi_relax, :cplex_relax, :cplex_fixed, :cplex_combi
 
     # define solution method for lagrangian dual
-    lagrangian_method = :bundle_level
+    lagrangian_method = :kelley
     # alternatives: :kelley, :bundle_proximal, :bundle_level
 
     bundle_alpha = 0.5
