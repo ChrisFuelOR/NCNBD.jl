7fc2c68cb4200efa67b6225f54968b3d945769a2
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2021-03-10 00:39:18 +0100

    Updated Lagrangian limit

--- examples/UnitCommitment/UC_5_10.jl
+++ examples/UnitCommitment/UC_5_10.jl
@@ -113,7 +113,7 @@ function unitCommitment_with_parameters(;
     epsilon_innerLoop::Float64 = 1e-2,
     lagrangian_atol::Float64 = 1e-4,
     lagrangian_rtol::Float64 = 1e-4,
-    lagrangian_iteration_limit::Int = 1000,
+    lagrangian_iteration_limit::Int = 10000,
     iteration_limit::Int=1000,
     time_limit::Int = 10800,
     sigma::Vector{Float64} = [0.0, 1000.0, 1000.0, 1000.0, 1000.0],
