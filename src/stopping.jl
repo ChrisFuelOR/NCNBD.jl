c33f242728f1a29d81fb7fb63b98e532e4c80bf9
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2021-03-09 17:58:49 +0100

    Improvement of stopping. Using best_upper_bound instead of upper_bound

--- src/stopping.jl
+++ src/stopping.jl
@@ -12,9 +12,9 @@ stopping_rule_status(::DeterministicStopping) = :DeterministicStopping
 
 function convergence_test(graph::SDDP.PolicyGraph, log::Vector{Log}, rule::DeterministicStopping, loop::Symbol)
 
-    bool_1 = abs(log[end].upper_bound - log[end].lower_bound)/abs(max(log[end].upper_bound, log[end].lower_bound)) <= log[end].opt_tolerance
+    bool_1 = abs(log[end].best_upper_bound - log[end].lower_bound)/abs(max(log[end].best_upper_bound, log[end].lower_bound)) <= log[end].opt_tolerance
     #bool_1 = log[end].upper_bound - log[end].lower_bound <= log[end].opt_tolerance
-    bool_2 = log[end].upper_bound - log[end].lower_bound >= -1e-4
+    bool_2 = log[end].best_upper_bound - log[end].lower_bound >= -1e-4
 
     return bool_1 && bool_2
 end
