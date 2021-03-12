dc1f06c7ee87377c268478a3cb9c4632ed1194ec
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2021-03-12 11:25:16 +0100

    Corrected dict

--- examples/UnitCommitment/UC_24_3.jl
+++ examples/UnitCommitment/UC_24_3.jl
@@ -53,7 +53,7 @@ function unitCommitment()
     sigma_factor = 2.0
 
     # define initial approximations
-    plaPrecision = Dict("valve" => [[1.0], [1.0], [1.0]], "emi" => [[0.226], [0.564], [0.646]])
+    plaPrecision = Dict(:valve => [[1.0], [1.0], [1.0]], :emi => [[0.226], [0.564], [0.646]])
     binaryPrecisionFactor = 1/7
 
     # define infiltration level
@@ -123,7 +123,7 @@ function unitCommitment_with_parameters(;
     time_limit::Int = 10800,
     sigma::Vector{Float64} = [0.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0, 2000.0],
     sigma_factor::Float64 = 2.0,
-    plaPrecision::Dict{Symbol,Array{Array{Float64,1},1}} = Dict("valve" => [[1.0], [1.0], [1.0]], "emi" => [[0.226], [0.564], [0.646]]), # apart from one generator always 1/5 of pmax
+    plaPrecision::Dict{Symbol,Array{Array{Float64,1},1}} = Dict(:valve => [[1.0], [1.0], [1.0]], :emi => [[0.226], [0.564], [0.646]]), # apart from one generator always 1/5 of pmax
     binaryPrecisionFactor::Float64 = 1/7,
     infiltrate_state::Symbol = :none, # alternatives: :none, :all, :outer, :sigma, :inner, :lagrange, :bellman
     dual_initialization_regime::Symbol = :zeros, # alternatives: :zeros, :gurobi_relax, :cplex_relax, :cplex_fixed, :cplex_combi
