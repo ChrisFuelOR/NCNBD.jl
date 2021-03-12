0a21c9c6cbf67bddd4f5f66167f22d4907ec386e
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2021-03-13 00:31:39 +0100

    Bundle test instanceStarter_1

--- examples/UnitCommitment/InstanceStarter_1.jl
+++ examples/UnitCommitment/InstanceStarter_1.jl
@@ -80,10 +80,10 @@ function start_instances()
                       # [UC_5_5, 1e-4, 1e-4, :none, :kelley, 0.0],
                       # [UC_5_5, 1e-4, 1e-4, :none, :bundle_level, 0.2],
 
-                      [UC_16_3, 1e-4, 1e-4, :none, :bundle_level, 0.2],
-                      [UC_24_3, 1e-4, 1e-4, :none, :bundle_level, 0.2],
-                      [UC_24_4, 1e-4, 1e-4, :none, :bundle_level, 0.2],
-                      [UC_36_3, 1e-4, 1e-4, :none, :bundle_level, 0.2],
+                      [UC_16_3, 1e-4, 1e-4, :none, :bundle_level, 0.2, 1e-2, 1e-2],
+                      [UC_24_3, 1e-4, 1e-4, :none, :bundle_level, 0.2, 1e-2, 1e-2],
+                      [UC_24_4, 1e-4, 1e-4, :none, :bundle_level, 0.2, 1e-2, 1e-2],
+                      [UC_36_3, 1e-4, 1e-4, :none, :bundle_level, 0.2, 1e-2, 1e-2],
 
                       ]
 
