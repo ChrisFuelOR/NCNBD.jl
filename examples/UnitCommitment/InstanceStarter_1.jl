cf97672a6011b6694b7eafe60787c301d8399268
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2021-03-13 00:26:12 +0100

    Bundle test instanceStarter_1

--- examples/UnitCommitment/InstanceStarter_1.jl
+++ examples/UnitCommitment/InstanceStarter_1.jl
@@ -19,6 +19,11 @@ include("UC_10_3.jl")
 include("UC_10_5.jl")
 include("UC_10_10.jl")
 
+include("UC_16_3.jl")
+include("UC_24_3.jl")
+include("UC_24_4.jl")
+include("UC_36_3.jl")
+
 function start_instances()
 
     # INSTANCE DEFINITIONS
@@ -57,7 +62,7 @@ function start_instances()
                       # #[UC_4_5, 1e-8, 1e-8, :none, :bundle_level, 0.2, 1e-2, 1e-2],
                       #[UC_4_5, 1e-4, 1e-4, :none, :kelley, 0.0, 1e-2, 1e-2],
                       #[UC_4_5, 1e-4, 1e-4, :none, :bundle_level, 0.2, 1e-2, 1e-2],
-                      [UC_3_10, 1e-4, 1e-4, :none, :bundle_level, 0.2, 1e-2, 1e-2],
+                      #[UC_3_10, 1e-4, 1e-4, :none, :bundle_level, 0.2, 1e-2, 1e-2],
                       #[UC_3_10, 1e-4, 1e-4, :none, :kelley, 0.0, 1e-2, 1e-2],
 
                       # #[UC_4_10, 1e-8, 1e-8, :none, :kelley, 0.0, 1e-2, 1e-2],
@@ -74,6 +79,11 @@ function start_instances()
                       # [UC_5_5, 1e-8, 1e-8, :cplex_combi, :bundle_level, 0.2],
                       # [UC_5_5, 1e-4, 1e-4, :none, :kelley, 0.0],
                       # [UC_5_5, 1e-4, 1e-4, :none, :bundle_level, 0.2],
+
+                      [UC_16_3, 1e-4, 1e-4, :none, :bundle_level, 0.2],
+                      [UC_24_3, 1e-4, 1e-4, :none, :bundle_level, 0.2],
+                      [UC_24_4, 1e-4, 1e-4, :none, :bundle_level, 0.2],
+                      [UC_36_3, 1e-4, 1e-4, :none, :bundle_level, 0.2],
 
                       ]
 
